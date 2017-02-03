bcf583547b0130396ea15c10081159e9c2419a2d
Admin Raisa <Admin Raisa>
2017-02-03 15:13:21 -0500

    add

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-var socket=io.connect('http://192.168.0.9:6677',{'forceNew':true});
+var socket=io.connect('http://192.168.20.167:6677',{'forceNew':true});
 
 socket.on('messages',function(data){
     //console.log(data);
@@ -31,10 +31,22 @@ function addMessage(e){
 
     var text = document.getElementById('text').value;
 
-    text = text.replace(':)', "<span class='emoticon feliz'></span>");
-    text = text.replace('::)', "<span class='emoticon triste'></span>");
-    text = text.replace('@@)', "<span class='emoticon amor'></span>");
 
+text = text.replace(":)emot10","<label class='emoticon-send emot10-send'></label>");
+text = text.replace(":)emot20","<label class='emoticon-send emot20-send'></label>");
+text = text.replace(":)emot30","<label class='emoticon-send emot30-send'></label>");
+text = text.replace(":)emot40","<label class='emoticon-send emot40-send'></label>");
+text = text.replace(":)emot50","<label class='emoticon-send emot50-send'></label>");
+text = text.replace(":)emot60","<label class='emoticon-send emot60-send'></label>");
+text = text.replace(":)emot70","<label class='emoticon-send emot70-send'></label>");
+text = text.replace(":)emot80","<label class='emoticon-send emot80-send'></label>");
+text = text.replace(":)emot90","<label class='emoticon-send emot90-send'></label>");
+text = text.replace(":)emot110","<label class='emoticon-send emot110-send'></label>");
+text = text.replace(":)emot120","<label class='emoticon-send emot120-send'></label>");
+text = text.replace(":)emot130","<label class='emoticon-send emot130-send'></label>");
+text = text.replace(":)emot140","<label class='emoticon-send emot140-send'></label>");
+text = text.replace(":)emot150","<label class='emoticon-send emot150-send'></label>");
+text = text.replace(":)emot160","<label class='emoticon-send emot160-send'></label>");
 
 
     var message = {
